8531893d0857d06ca0c31942e6427fe3cf7c41c7
Tsz Li <btli@ymail.com>
2020-02-10 16:31:32 -0800

    first version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import './App.css'
-
+import './App.css';
+var shake = ["", "shaking"]
+var i = 0
+var myVar
 class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
       // Add magic 8 ball outcomes to the array
-      answerArray: [],
+      answerArray: ["It is certain", "It is decidedely so", "Without a dobut", "Yes - definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"],
       question: ""
     }
   }
@@ -14,9 +16,15 @@ class App extends React.Component {
   getAnswer = () => {
     // destructures answerArray out of state
     const { answerArray } = this.state
-    // write your code here!
+    const { question } = this.state
+    if (question.charAt(question.length-1) === "?"){
+        let ourAnswer = answerArray[(Math.floor(Math.random()* answerArray.length))]
+        return ourAnswer
+    } else {
+        return "Please ask a question"
+    }
+    // write your code here
     // Update the return statement below to reflect the outcome of your code.
-    return this.state.question
   }
 
   handleChange = (e) => {
@@ -30,6 +38,8 @@ class App extends React.Component {
     // calls the getAnswer function and saves the outcome as answer
     const answer = this.getAnswer()
     this.setState({ answer: answer })
+    i = 1
+
   }
 
   render(){
@@ -45,7 +55,9 @@ class App extends React.Component {
         <button onClick={ this.handleSubmit }>
           Ask the Magic 8 Ball a Question
         </button>
-        <p> { this.state.answer } </p>
+        <p class = "answer"> { this.state.answer } </p>
+        <img id = {shake[i]} src={require("./8ball-pixilart.png")} width="50%" height="50%" alt="8 ball image"></img>
+
       </div>
     )
   }
